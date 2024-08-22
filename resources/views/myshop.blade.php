3f9dfcc8c726d9b2c387b0b340f4727f5460f3aa
Tee <theerathat.m@kkumail.com>
2024-08-22 17:35:15 +0700

    itshop2

--- resources/views/myshop.blade.php
+++ resources/views/myshop.blade.php
@@ -1,17 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>เกี่ยวกับร้าน IT Shop</h1>
-    <ul>
-        <li>ที่อยู่ร้าน: {{$address}} </li>
-        <li>เบอร์โทร: {{$mobilephone}}</li>
-        <li>ที่ตั้งร้าน: {{$location}} </li>
-    </ul>
-    
-</body>
-</html>
+
+@extends('layouts.myapp')
+
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h6>เกี่ยวกับร้าน IT Shop</h6>
+                </div>   
+
+                <div class="card-body">
+                    <ul>
+                       <li>ที่อยู่ร้าน: {{$address}} </li>
+                       <li>เบอร์โทร: {{$mobilephone}}</li>
+                       <li>ที่ตั้งร้าน: {{$location}} </li>
+                    </ul>
+                </div>  
+            </div>     
+        </div>            
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,23 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\WebController;
-
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 Route::get('/myshop', [WebController::class, 'myshop'])->name('web.myshop');
+
+
+require __DIR__.'/auth.php';
